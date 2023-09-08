67cc989c1b49d4b0873a45e27f593509f5103579
niloofarizadyar <niloofarizadyar1375@gmail.com>
2023-09-08 20:27:03 +0330

    work-logger is modified

--- src/app/view/work-logger/work-logger.component.ts
+++ src/app/view/work-logger/work-logger.component.ts
@@ -5,39 +5,30 @@ import { HolidayService } from 'src/app/services/holiday.service';
 @Component({
   selector: 'app-work-logger',
   templateUrl: './work-logger.component.html',
-  styleUrls: ['./work-logger.component.scss']
+  styleUrls: ['./work-logger.component.scss'],
 })
 export class WorkLoggerComponent {
   holidays: any[] = [];
-  newHoliday: any = {
-    name: "new one",
-    date: "2023-4-6"
-  }
-  
-  constructor(public holiday: HolidayService){
+
+  constructor(public holiday: HolidayService) {
     this.getHolidays();
   }
-  
+
   name = new FormControl('', [Validators.required, Validators.minLength(3)]);
-  date = new FormControl('',[Validators.required])
+  date = new FormControl('', [Validators.required]);
   holidayForm = new FormGroup({
     name: this.name,
-    date: this.date
+    date: this.date,
   });
-
 
   getHolidays() {
     this.holiday.getHolidays().subscribe((data) => {
       this.holidays = data;
-      console.log(this.holidays)
+      console.log(this.holidays);
     });
   }
-  postHoliday(){
-    this.holiday.createHoliday(this.holidayForm.value)
+  postHoliday() {
+    this.holiday.createHoliday(this.holidayForm.value);
     this.holidayForm.reset();
   }
-  // deleteHoliday(){
-  //   this.holiday.deleteHoliday
-  // }
-  
 }
