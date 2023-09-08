8b2c04a2f8b899322e97654fc00fa848e1d26a95
niloofarizadyar <niloofarizadyar1375@gmail.com>
2023-09-08 18:08:48 +0330

    table of holiday is added

--- src/app/services/holiday.service.ts
+++ src/app/services/holiday.service.ts
@@ -1,9 +1,66 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { faker } from '@faker-js/faker';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HolidayService {
+  private mockHolidays: any[] = [];
 
-  constructor() { }
+  constructor() {
+    this.generateMockHolidays();
+  }
+
+  private generateMockHolidays() {
+    this.mockHolidays = Array.from({ length: 10 }, () => ({
+      name: faker.lorem.words(2),
+      date: faker.date.future(),
+    }));
+  }
+  //GET Holiday
+  getHolidays(): Observable<any[]> {
+    const event = new Date('2023-09-08');
+    console.log(event.toUTCString());
+
+    return of(this.mockHolidays);
+  }
+  //POST Holiday
+  createHoliday(newHoliday: any): Observable<any> {
+    const existingHoliday = this.mockHolidays.find(
+      (h) => h.date === newHoliday.date
+    );
+
+    if (existingHoliday) {
+      return of(null);
+    } else {
+      this.mockHolidays.push(newHoliday);
+      return of(newHoliday);
+    }
+  }
+  //DELETE Holiday
+  deleteHoliday(date: string): Observable<any> {
+    const index = this.mockHolidays.findIndex((h) => h.date === date);
+
+    if (index !== -1) {
+      const deletedHoliday = this.mockHolidays.splice(index, 1)[0];
+      return of(deletedHoliday);
+    } else {
+      return of(null);
+    }
+  }
+  //irst need toggle to form, then change them
+  //PUT Holiday
+  updateHoliday(updatedHoliday: any): Observable<any> {
+    const index = this.mockHolidays.findIndex(
+      (h) => h.date === updatedHoliday.date
+    );
+
+    if (index !== -1) {
+      this.mockHolidays[index].name = updatedHoliday.name;
+      return of(this.mockHolidays[index]);
+    } else {
+      return of(null);
+    }
+  }
 }
--- src/app/view/work-logger/work-logger.component.ts
+++ src/app/view/work-logger/work-logger.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HolidayService } from 'src/app/services/holiday.service';
 
 @Component({
   selector: 'app-work-logger',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./work-logger.component.scss']
 })
 export class WorkLoggerComponent {
-
+  holidays: any[] = [];
+  newHoliday: any = {
+    name: "new one",
+    date: "2023-4-6"
+  }
+  constructor(public holiday: HolidayService){
+    this.getHolidays();
+  }
+  getHolidays() {
+    this.holiday.getHolidays().subscribe((data) => {
+      this.holidays = data;
+      console.log(this.holidays)
+    });
+  }
+  postHoliday(){
+    this.holiday.createHoliday(this.newHoliday)
+  }
+  // deleteHoliday(){
+  //   this.holiday.deleteHoliday
+  // }
 }
