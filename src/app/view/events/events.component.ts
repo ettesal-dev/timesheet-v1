7c77f6c813b382f7c5a4e137369e6febef19beae
niloofarizadyar <niloofarizadyar1375@gmail.com>
2023-09-17 17:42:38 +0330

    event template is modified

--- src/app/view/events/events.component.ts
+++ src/app/view/events/events.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormArray, FormBuilder } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormArray,
+  FormBuilder,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-events',
@@ -7,8 +13,25 @@ import { FormGroup, FormControl, Validators, FormArray, FormBuilder } from '@ang
   styleUrls: ['./events.component.scss'],
 })
 export class EventsComponent {
-  
-  events:any[] = []
+  //for event table and GET events
+  events: any[] = [{
+    id:12,
+    creatorName: "John Doe",
+    eventName: "My Event",
+    eventDate: new Date('2023-12-01'),
+    startTime: "start",
+    endTime: "end",
+    attendees:["user 1","user 2", "user 3"]
+  }];
+  //for users from keycloak
+  users: any[] = ["user 1", "user 2", "user 3"]
+  updateParameters:any = {
+    eventId: "",
+    eventDate:"",
+    eventStart: "",
+    eventEnd:""
+  }
+
   constructor(private fb: FormBuilder) {}
 
   //get data from form for post new holiday
@@ -23,10 +46,10 @@ export class EventsComponent {
   eventDate = new FormControl('', [Validators.required]);
   startTime = new FormControl('', [Validators.required]);
   endTime = new FormControl('', [Validators.required]);
-  attendeesName = new FormControl('', [Validators.required])
-  
+  attendeesName = new FormControl('', [Validators.required]);
+
   attendeesForm = this.fb.group({
-    attendeesName: this.attendeesName
+    attendeesName: this.attendeesName,
   });
 
   eventForm = this.fb.group({
@@ -35,28 +58,88 @@ export class EventsComponent {
     eventDate: this.eventDate,
     startTime: this.startTime,
     endTime: this.endTime,
-    attendees: this.fb.array([]),
+    attendees: this.fb.array([], Validators.required),
+  });
+  //for update attendees
+  updateAttendeesForm = this.fb.group({
+    name: new FormControl('', [Validators.required]),
   });
 
-  get attendees(){
+  updatedEventForm = this.fb.group({
+    updatedAttendees: this.fb.array([],Validators.required),
+  });
+
+  // get updatedAttendees() {
+  //   return this.updatedEventForm.controls['updatedAttendees'] as FormArray;
+  // }
+
+  get attendees() {
     return this.eventForm.controls['attendees'] as FormArray;
   }
 
-  deleteAttendees(index:number){
-
-  }
-
-  postEvent() {
-    
-    
-  }
-  addToName() {
-    //const newName = new FormControl("my name");
-    //this.attendees.push(newName)
+  addAttendees() {
     const ad = this.fb.group({
       attendeesName: new FormControl('', [Validators.required]),
     });
-    
+
     this.attendees.push(ad);
+  }
+
+  deleteAttendees(index: number) {
+    this.attendees.removeAt(index);
+  }
+
+  get updatedAttendees() {
+    return this.updatedEventForm.controls['updatedAttendees'] as FormArray;
+  }
+
+  addUpdatedAttendees() {
+    const ad = this.fb.group({
+      name: new FormControl('', [Validators.required]),
+    });
+
+    this.updatedAttendees.push(ad);
+  }
+
+  deleteUpdatedAttendees(id: number) {
+    this.updatedAttendees.removeAt(id);
+  }
+  //post method
+  postEvent() {
+    const result = {
+      creatorName: this.eventForm.value.creatorName,
+      eventName: this.eventForm.value.eventName,
+      eventDate: this.eventForm.value.eventDate,
+      startTime: this.eventForm.value.startTime,
+      endTime: this.eventForm.value.endTime,
+      attendees: this.eventForm.value.attendees?.map(
+        (obj: any) => obj.attendeesName
+      ),
+    };
+    console.log(result);
+  }
+  
+  //find id by putEvent(event.id, event.date, event.start, event.end) in template
+  updateEvent(eventID:any, eventDate:string, eventStart:string, eventEnd:string){
+    this.updateParameters.eventId = eventID
+    this.updateParameters.eventDate = eventDate
+    this.updateParameters.eventStart = eventStart
+    this.updateParameters.eventEnd = eventEnd
+    console.log(this.updateParameters)
+  }
+  putEvent(){
+    //give updateParameters to PUT method service
+    if (this.updatedEventForm.valid) {
+      const result = this.updatedEventForm.value.updatedAttendees?.map((obj: any) => obj.name);
+      //result as request body
+      console.log(result);
+    } else {
+      this.updatedEventForm.markAllAsTouched();
+    }
+  
+  }
+  //find id by deleteEvent(event.id) in template
+  deleteEvent(id:any){
+    
   }
 }
